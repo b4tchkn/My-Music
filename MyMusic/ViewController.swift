79ed7fc0531f917542d83b617c800db15f113001
Kakeru Nakabachi <b1015146@fun.ac.jp>
2018-03-14 01:47:26 +0900

    BackMusic Play & Stop

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -38,5 +38,51 @@ class ViewController: UIViewController {
             print("cymbal error")
         }
     }
+    //ギター音源ファイルを指定
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    
+    //ギター用のプレイヤーインスタンス作成
+    var guitarPlayer = AVAudioPlayer()
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            //ギター用のプレイヤーに音源ファイルを指定
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            
+            guitarPlayer.play()
+        } catch {
+            print("guitar error")
+        }
+    }
+    
+    let backMusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    var backMusicPlayer = AVAudioPlayer()
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            backMusicPlayer = try AVAudioPlayer(contentsOf: backMusicPath, fileTypeHint: nil)
+            backMusicPlayer.numberOfLoops = -1
+            backMusicPlayer.play()
+        } catch {
+            print("backmusic error")
+        }
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        //バックミュージック停止
+        backMusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("caused error")
+        }
+    }
+    
 }
 
